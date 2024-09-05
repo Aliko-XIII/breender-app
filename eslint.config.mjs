2a4d5bdc1a760103ce233422a81bd09092d82038
Aliko-XIII <tektov2004@gmail.com>
2024-09-05 11:35:12 +0300

    eslint ignores dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,7 @@ import globals from "globals";
 
 export default [
   {
-    ignores: ['node_modules', 'frontend/dist/*', 'tests/*'],
+    ignores: ['node_modules', 'frontend/dist/*', 'tests/*', 'dist/*'],
   },
   {
     files: ["**/*.js"],
