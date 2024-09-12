7829e66fc0c2105b748eff3d7980f3b7903f0bb7
Aliko-XIII <tektov2004@gmail.com>
2024-09-12 09:06:15 +0300

    cors handler

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,18 @@ import path from 'path';
 const app = express();
 app.use(express.json());
 
+//CORS handler
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    if (req.method === 'OPTIONS') {
+      return res.status(200).end();
+    }
+    next();
+  });
+
 
 //Server listening port
 app.listen(process.env.PORT, () => {
