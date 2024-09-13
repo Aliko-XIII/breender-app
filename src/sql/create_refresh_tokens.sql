d55f1988497ff79506120ff6a34c0e09e084dc16
Aliko-XIII <tektov2004@gmail.com>
2024-09-13 10:28:19 +0300

    changed tokens table create script timestamp to seconds bigint

--- src/sql/create_refresh_tokens.sql
+++ src/sql/create_refresh_tokens.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS refresh_tokens
 (
     user_id uuid NOT NULL,
     refresh_token character varying,
-    expires_at timestamp with time zone,
+    expires_at BIGINT,
     CONSTRAINT refresh_token_pkey PRIMARY KEY (user_id),
     CONSTRAINT user_FK FOREIGN KEY (user_id)
         REFERENCES users (user_id) MATCH SIMPLE
