dc1be8929f9f1b0ab3d83daef147245afd24af44
Aliko-XIII <tektov2004@gmail.com>
2024-11-25 19:53:13 +0200

    modifies create animal dto

--- server-breender/src/animals/dto/create-animal.dto.ts
+++ server-breender/src/animals/dto/create-animal.dto.ts
@@ -1,21 +1,36 @@
-import { IsString, IsDateString, IsEnum, IsUUID } from 'class-validator';
+import {
+  IsString,
+  IsDateString,
+  IsEnum,
+  IsUUID,
+  MinLength,
+  MaxLength,
+  IsOptional,
+} from 'class-validator';
 import { Sex } from '@prisma/client';
 
 export class CreateAnimalDto {
   @IsString()
+  @MinLength(2, { message: 'Name must be at least 2 characters long' })
+  @MaxLength(50, { message: 'Name must not exceed 50 characters' })
   name: string;
 
-  @IsEnum(Sex)
+  @IsEnum(Sex, { message: 'Invalid sex value' })
   sex: Sex;
 
-  @IsString()
+  @IsString({ message: 'Breed must be a string' })
+  @MinLength(2, { message: 'Breed must be at least 2 characters long' })
+  @MaxLength(50, { message: 'Breed must not exceed 50 characters' })
   breed: string;
 
-  @IsString()
+  @IsString({ message: 'Species must be a string' })
+  @MinLength(2, { message: 'Species must be at least 2 characters long' })
+  @MaxLength(50, { message: 'Species must not exceed 50 characters' })
   species: string;
 
   @IsString()
-  bio: string;
+  @IsOptional()
+  bio?: string;
 
   @IsDateString()
   birthDate: Date;
