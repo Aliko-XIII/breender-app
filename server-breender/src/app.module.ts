8d635598b6616ac0e5cd73d343b2c5494b565cdd
Aliko-XIII <tektov2004@gmail.com>
2024-11-25 19:52:30 +0200

    auth additional

--- server-breender/src/app.module.ts
+++ server-breender/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { AnimalsModule } from './animals/animals.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, AnimalsModule],
+  imports: [DatabaseModule, UsersModule, AnimalsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
