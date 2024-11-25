4bcc48e45c3f43a50bc784fee834709a89ec6a41
Aliko-XIII <tektov2004@gmail.com>
2024-11-25 19:51:45 +0200

    user module setup

--- server-breender/src/users/dto/update-user.dto.ts
+++ server-breender/src/users/dto/update-user.dto.ts
@@ -1,4 +1,37 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString, IsEmail, MinLength, IsStrongPassword, MaxLength } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @IsOptional()
+  @IsEmail()
+  email?: string;
+
+  @IsOptional()
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minNumbers: 1,
+      minLowercase: 1,
+      minUppercase: 1,
+      minSymbols: 1,
+    },
+    { message: 'Password is not strong enough' },
+  )
+  @MaxLength(30, { message: 'Password must not exceed 30 characters' })
+  pass?: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(50, { message: 'Name must not exceed 50 characters' })
+  name?: string;
+
+  @IsOptional()
+  @IsString()
+  bio?: string;
+
+  @IsOptional()
+  @IsString()
+  @MinLength(2, {message: 'Picture URL is not valid'})
+  pictureUrl?: string;
+}
--- server-breender/src/users/users.service.ts
+++ server-breender/src/users/users.service.ts
@@ -1,26 +1,140 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { DatabaseService } from 'src/database/database.service';
+import { Prisma } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private databaseService: DatabaseService) { }
+
+  /**
+   * Generates a bcrypt hash of the provided password.
+   * @param {string} password - The plain text password to hash.
+   * @returns {Promise<string>} - A promise that resolves to the hashed password using bcrypt.
+   */
+  private async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    const salt = await bcrypt.genSalt(saltRounds);
+    return bcrypt.hash(password, salt);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  /**
+   * Compares a plain text password with a bcrypt-hashed password.
+   * @param {string} password - The plain text password to compare.
+   * @param {string} hashedPassword - The previously hashed password for comparison.
+   * @returns {Promise<boolean>} - A promise that resolves to true if passwords match, otherwise false.
+   */
+  private async comparePasswords(
+    password: string,
+    hashedPassword: string,
+  ): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  /**
+   * Validates user credentials.
+   * @param {string} email - The email of the user attempting to authenticate.
+   * @param {string} password - The plain text password to validate against the stored hash.
+   * @returns {Promise<UserSafeDto>} - A promise that resolves to the updated user's safe DTO.
+   * @throws {NotFoundException} - If no user is found with the given username.
+   * @throws {BadRequestException} - If the provided password is invalid.
+   */
+  public async checkAuth(email: string, password: string): Promise<boolean> {
+    const user = await this.databaseService.user.findUnique({
+      where: { email: email },
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found.');
+    }
+
+    const isPasswordValid = await this.comparePasswords(
+      password,
+      user.hashedPass,
+    );
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid password.');
+    }
+
+    return true;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async create(createUserDto: CreateUserDto) {
+    const existingUser = await this.databaseService.user.findUnique({
+      where: { email: createUserDto.email },
+    });
+
+    if (existingUser) {
+      throw new Error('Email is already in use');
+    }
+
+    const hashedPass = await this.hashPassword(createUserDto.pass);
+
+    const registeredUser: Prisma.UserCreateInput = {
+      email: createUserDto.email,
+      hashedPass: hashedPass,
+    };
+
+    return await this.databaseService.user.create({ data: registeredUser });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findAll() {
+    return await this.databaseService.user.findMany({});
+  }
+
+  async findOne(id: string) {
+    return await this.databaseService.user.findUnique({ where: { id } });
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const updatedUser: Prisma.UserUpdateInput = {};
+    if (updateUserDto.email) {
+      updatedUser.email = updateUserDto.email;
+    }
+    if (updateUserDto.pass) {
+      const hashedPass = await this.hashPassword(updateUserDto.pass);
+      updatedUser.hashedPass = hashedPass;
+    }
+
+    try {
+      await this.databaseService.user.update({
+        data: updatedUser,
+        where: { id },
+      });
+    } catch (error) {
+      console.error(`Failed to update user login data with ID ${id}:`, error);
+      throw new Error(`Could not update user login data.`);
+    }
+
+    const updatedProfile: Prisma.UserProfileUpdateInput = {};
+    if (updateUserDto.bio) {
+      updatedProfile.bio = updateUserDto.bio;
+    }
+    if (updateUserDto.name) {
+      updatedProfile.name = updateUserDto.name;
+    }
+    if (updateUserDto.pictureUrl) {
+      updatedProfile.pictureUrl = updateUserDto.pictureUrl;
+    }
+
+    try {
+      await this.databaseService.userProfile.update({
+        data: updatedProfile,
+        where: { userId: id },
+      });
+    } catch (error) {
+      console.error(`Failed to update profile for user with ID ${id}:`, error);
+      throw new Error(`Could not update profile for user.`);
+    }
+  }
+
+  async remove(id: string) {
+    return await this.databaseService.user.delete({ where: { id } });
   }
 }
