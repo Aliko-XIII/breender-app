157ee4e799dfaa9c7a772cb42c32e5f3c1625df6
Aliko-XIII <tektov2004@gmail.com>
2024-11-30 18:09:51 +0200

    adds simple welcome page landing

--- client-breender/src/components/WelcomePage/WelcomePage.tsx
+++ client-breender/src/components/WelcomePage/WelcomePage.tsx
@@ -1,8 +1,21 @@
 import './WelcomePage.scss';
 
-export const WelcomePage = ()=>{
-
-    return <>
-    
-    </>
-}
+export const WelcomePage = () => {
+    return (
+      <div className='welcome-block'>
+        <h1>Welcome to the Animal Breeder App <span>"Breender"</span></h1>
+        <p>
+          Manage your animal breeding operations seamlessly with our powerful and user-friendly application. Whether you're maintaining records, organizing care, or finding breeding partners, we've got you covered.
+        </p>
+        <ul>
+          <li>🗂️ Add, update, delete, and manage user and animal data.</li>
+          <li>📄 Upload important documents, pedigrees, and other animal-related data.</li>
+          <li>🛠️ Organize and track animal care routines.</li>
+          <li>🐾 Find compatible breeding partners for your animals.</li>
+        </ul>
+        <p>
+          Start building better connections and ensuring the best care for your animals today!
+        </p>
+      </div>
+    );
+  };
