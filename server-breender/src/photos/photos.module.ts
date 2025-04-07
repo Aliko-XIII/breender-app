4f07ae8dc3d7e892bb9637c720aa02e6c3a23c5f
Aliko-XIII <tektov2004@gmail.com>
2025-04-08 02:12:16 +0300

    fix: imports jwt to photos module

--- server-breender/src/photos/photos.module.ts
+++ server-breender/src/photos/photos.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { PhotosController } from './photos.controller';
 import { PhotosService } from './photos.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [PhotosController],
   providers: [PhotosService]
 })
-export class PhotosModule {}
+export class PhotosModule { }
