83d8ba10f14a511ecbdc24550bad24757977965a
Aliko-XIII <tektov2004@gmail.com>
2025-04-09 00:27:01 +0300

    adds dtos for reminders

--- server-breender/src/reminders/dto/createReminder.dto.ts
+++ server-breender/src/reminders/dto/createReminder.dto.ts
@@ -1,3 +1,23 @@
-export class CreateReminderDto{
+import { ReminderType } from "@prisma/client";
+import { IsDate, IsEnum, IsOptional, IsString, IsUUID } from "class-validator";
+
+export class CreateReminderDto {
+    @IsUUID()
+    id: string;
+
+    @IsUUID()
+    userId: string;
     
+    @IsUUID()
+    animalId: string;
+    
+    @IsEnum(ReminderType)
+    reminderType: ReminderType;
+
+    @IsOptional()
+    @IsString()
+    message: string;
+
+    @IsDate()
+    remindAt: Date;
 }
--- server-breender/src/reminders/dto/updateReminder.dto.ts
+++ server-breender/src/reminders/dto/updateReminder.dto.ts
@@ -1,3 +1,16 @@
-export class UpdateReminderDto{
-    
+import { ReminderType } from "@prisma/client";
+import { IsDate, IsEnum, IsOptional, IsString, IsUUID } from "class-validator";
+
+export class UpdateReminderDto {
+    @IsOptional()
+    @IsEnum(ReminderType)
+    reminderType?: ReminderType;
+
+    @IsOptional()
+    @IsString()
+    message?: string;
+
+    @IsOptional()
+    @IsDate()
+    remindAt?: Date;
 }
