d65a682fbb9ae09d200ed54375a9b009edf17df7
Aliko-XIII <tektov2004@gmail.com>
2025-04-09 00:28:02 +0300

    removes unnecessary fields from createreminder dto

--- server-breender/src/reminders/dto/createReminder.dto.ts
+++ server-breender/src/reminders/dto/createReminder.dto.ts
@@ -1,13 +1,7 @@
 import { ReminderType } from "@prisma/client";
 import { IsDate, IsEnum, IsOptional, IsString, IsUUID } from "class-validator";
 
-export class CreateReminderDto {
-    @IsUUID()
-    id: string;
-
-    @IsUUID()
-    userId: string;
-    
+export class CreateReminderDto {  
     @IsUUID()
     animalId: string;
     
