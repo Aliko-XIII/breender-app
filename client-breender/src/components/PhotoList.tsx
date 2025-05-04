f1531fad189e9b17f6081e435c7ec6f55079263b
Aliko-XIII <tektov2004@gmail.com>
2025-05-04 23:19:43 +0300

    feat: Update delete button in PhotoList component with icon and improved styling

--- client-breender/src/components/PhotoList.tsx
+++ client-breender/src/components/PhotoList.tsx
@@ -84,12 +84,15 @@ const PhotoList: React.FC<PhotoListProps> = ({ animalId, userId }) => {
             <div className="card h-100 photo-card" style={{ cursor: 'pointer', position: 'relative' }} onClick={() => handlePhotoClick(photo)}>
               <button
                 type="button"
-                className="btn btn-danger btn-sm position-absolute end-0 m-2"
-                style={{ zIndex: 2 }}
+                className="btn btn-outline-danger btn-sm rounded-circle position-absolute end-0 m-2 d-flex align-items-center justify-content-center"
+                style={{ zIndex: 2, width: 32, height: 32 }}
                 onClick={e => { e.stopPropagation(); handleDeletePhoto(photo.id); }}
                 title="Delete photo"
               >
-                &times;
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5.5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6zm2 .5a.5.5 0 0 1 .5-.5.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6z"/>
+                  <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1 0-2h3a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3a1 1 0 0 1 1 1zm-11-1a.5.5 0 0 0-.5.5V4h11V2.5a.5.5 0 0 0-.5-.5h-10zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118z"/>
+                </svg>
               </button>
               <img
                 src={API_BASE_URL + photo.photoUrl}
